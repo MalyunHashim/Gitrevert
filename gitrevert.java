458f89a207051a14c8a7e6e46eedf7405bb1be15
malyumohamed <malyunmohaz942@gmail.com>
2023-06-15 18:19:28 +0300

    printed statement

--- gitrevert.java
+++ gitrevert.java
@@ -4,8 +4,14 @@ public class gitrevert {
     public static void main(String[] args) {
         int x =34;
         int y = 45;
+        //this addition
         System.out.println(x+y);
+
+        //this multipilication
         System.out.println(x*y);
         
+        //this subtraction
+
+        System.out.println(x-y);
     }
 }
